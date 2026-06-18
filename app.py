6695d17892ceaf97a04e31ff2310b2a594f2fa06
Rekha Sri <kotcherlarekha44@gmail.com>
2026-06-19 02:55:51 +0530

    Added Jinja2 templating, inheritance, filters, and redirects

--- app.py
+++ app.py
@@ -1,58 +1,36 @@
-from flask import Flask, request, make_response
-app = Flask(__name__)
+from flask import Flask, render_template, url_for, redirect
+app = Flask(__name__, template_folder='templates')
  
 @app.route('/')
 def index():
-    return "Hello World"
-    #also it could be changed into html - return "<h1>hello world</h1>"
-
-@app.route('/abc')
-def hello():
-        response = make_response('hehehheee\n')
-        response.status_code = 202
-        response.headers['content-type'] = 'application/octet-stream'
-        return response
-
-@app.route('/greet/<name>')
-def greet(name):
-    return f"helloo {name}"
-
-@app.route('/add/<number1>/<number2>')
-def add(number1, number2):
-    return f"{number1} + {number2} = {number1 + number2}"
-
-
-
-@app.route('/handle_url_params')
-def handle_params():
-    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
-        greeting = request.args.get('greeting')
-        name = request.args.get('name')
-        return f"{greeting}, {name}"
-    else:
-        return 'some parameters are missing'
+    myvalue = "goood"
+    myresult = 10 + 20
+    mylist = [10,20,30]
     
+    return render_template('index.html',myvalue=myvalue,myresult=myresult,mylist=mylist)
+    
+@app.route('/other')
+def other():
+    some_text = "hello flask world"
+    return render_template('other.html',some_text=some_text)
 
-@app.route('/greet_params')
-def greet_params():
-    greeting = request.args.get('greeting')
-    name = request.args.get('name')
-    return f"{greeting}, {name}"
+@app.template_filter('reverse_string')
+def reverse_string(s):
+    return s[::-1]
 
-#to consider this concatenated string as actual numerical calculation - @app.route('/add/<int:number1>/<int:number2>') (typecasting)
+@app.template_filter('repeat')
+def repeat(s, times=2):
+    return s*times
+
+@app.template_filter('alternate_case')
+def alternate_case(s):
+    return ''.join([c.upper() if i % 2 == 0 else c.lower() for i,c in enumerate(s)])
+
+@app.route('/redirect_endpoint')
+def redirect_endpoint():
+    return redirect(url_for('other'))
+
+
 if __name__ == '__main__': 
     app.run(host='0.0.0.0',port = 5555, debug = True)
 
-
-#------------------------------------------------------------------------
-#Browser:  http://127.0.0.1:5555/
-#Output:  Hello World
-
-#Browser:  http://127.0.0.1:5555/abc
-#Output:  hehe
-
-#Browser:  http://127.0.0.1:5555/greet/Rekhss
-#Output: helloo Rekhss
-
-#Browser: http://127.0.0.1:5555/handle_url_params?greeting=Hello&name=Rekhss
-#Output:Hello, Rekhss
