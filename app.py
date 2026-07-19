798b83796d0041d0d0e2f20ec2ace8203ddd8591
Rekha Sri <167110296+rekhasri44@users.noreply.github.com>
2026-07-19 14:55:56 +0530

    Implement file upload route with content type checks
    Added file upload functionality to handle text and Excel files.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__, template_folder='templates')
 
@@ -14,9 +15,16 @@ def index():
        else:
           return 'failure'
 
-@app.route('/file_upload')
-def file file_upload():
-    return ""
+@app.route('/file_upload', methods=["POST"])
+def file_upload():
+    file = request.files('file")
+
+    if file.content_type == 'text/plain':
+                         return file.read().decode()
+    elif file.content_type == 'application/vmd.openxmlformats-officedocument.spreadsheetml.sheet or file.content_type == 'application/vmd.ms-excel, text/plain'
+                         df = odf.read_excel(file)
+                         return df.to_html()
+
 if __name__ == '__main__': 
     app.run(host='0.0.0.0',port = 5555, debug = True)
 
