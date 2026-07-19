516e64fdfa09739fab3fca150c18186c8da1b5d0
Rekha Sri <167110296+rekhasri44@users.noreply.github.com>
2026-07-19 15:09:12 +0530

    Enhance file upload functionality in app.py
    Refactor app.py to handle file uploads and improve request handling.

--- app.py
+++ app.py
@@ -1,22 +1,39 @@
-from flask import Flask, render_template, url_for, redirect
-app = Flask(__name__, template_folder='templates')
+import pandas as pd
+from flask import Flask, render_template, request
 
-@app.route(rule: '/', methods=['GET', 'POST'])
+app = Flask(__name__, template_folder="templates")
+
+
+@app.route("/")
 def index():
-    if request.method == 'GET':
-       return render_template('index.html')
-     else request.method == 'POST':
-       username = request.form.get("username")
-       password = request.form.get("password")
+    return render_template("index.html")
 
-       if username == 'rekha_kprs" and password == "poorna":
-          return 'success'
-       else:
-          return 'failure'
 
-@app.route('/file_upload')
-def file file_upload():
-    return ""
-if __name__ == '__main__': 
-    app.run(host='0.0.0.0',port = 5555, debug = True)
+@app.route("/file_upload", methods=["GET", "POST"])
+def file_upload():
+
+    if request.method == "GET":
+        return render_template("upload.html")
+
+    file = request.files.get("file")
+
+    if file is None:
+        return "No file uploaded"
+
+    if file.content_type == "text/plain":
+        return file.read().decode()
+
+    elif file.content_type in [
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        "application/vnd.ms-excel",
+    ]:
+        df = pd.read_excel(file)
+        return df.to_html()
+
+    else:
+        return "Unsupported file type"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5555, debug=True)
 
